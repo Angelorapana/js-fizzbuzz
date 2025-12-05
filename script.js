ae82e4debd2e6838ecf323f2f7a66b30e292d7c4
angelorapana <angelorapana@icloud.com>
2025-12-05 15:51:18 +0100

    creazione struttura codice

--- script.js
+++ script.js
@@ -9,7 +9,22 @@
 // proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 
 
+//Come faccio a sapere se un numero è divisibile per?
+//Quando il risultato della divisione di numero / 3 o / 5 è un numero intero
+
+let numero
+let risultatoDivisoTre = (numero/3);
+let risultatoDivisoCinque = (numero/5);
+
+
 
 for (let numero = 1; numero <= 100; numero = numero +1 ){
-    console.log(numero);  
+    
+    let risultatoDivisoTre = (numero/3);
+    let risultatoDivisoCinque = (numero/5);
+
+    if (risultatoDivisoTre == 3){console.log(numero + "Fizz")}
+      else if (risultatoDivisoCinque == 5){console.log(numero + "Buzz")}
+       else if (risultatoDivisoCinque && risultatoDivisoTre == 15) { console.log (numero + "FizzBuzz")}
+       else if (risultatoDivisoCinque && risultatoDivisoTre != 15 , 5 , 3){console.log(numero)}
 }
